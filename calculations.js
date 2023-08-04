1cd08ce6da2a6179e2af9f514222abe4fd5eb051
[Vasily Shabrov] <[bazzone21@gmail.com]>
2023-08-05 00:52:52 +0200

    HW6.2(add assignment operators)

--- calculations.js
+++ calculations.js
@@ -1,32 +1,29 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 
 function decreaseBalance(x, y) {
   //здесь не хватало второй переменной 'Y'
-  x = x - y;
+  x -= y;
   return x;
 }
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
+  x /= y;
   return x;
   console.log("x"); // - непонятно, для чего после return выводить x в consol.log, после return выполнение кода останавливается
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
--(
-  //здесь видимо лишнее третье число 4000
-  console.log(getRestAfterDivision(7000, 3))
-);
+console.log(divideBalanceByAccounts(3000, 2, 4000)); //здесь видимо лишнее третье число 4000
+console.log(getRestAfterDivision(7000, 3));
 
 //[Running] node "/Users/vasilijsabrov/Documents/HW6.1/hw-js-assignments/tempCodeRunnerFile.js"
 //3700
